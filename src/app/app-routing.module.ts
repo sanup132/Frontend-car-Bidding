b13b0f54746cffab90b364db8283741d808f5270
Sanup <sanup.cse@gmail.com>
2022-11-09 15:01:44 +0530

    upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,52 @@
+import { createMayBeForwardRefExpression } from '@angular/compiler';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddCarComponent } from './components/add-car/add-car.component';
+import { AdminLoginComponent } from './components/admin-login/admin-login.component';
+import { CarInBiddingComponent } from './components/car-in-bidding/car-in-bidding.component';
+import { CarSoldComponent } from './components/car-sold/car-sold.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { OwnerDetailsComponent } from './components/owner-details/owner-details.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BiddingPriceComponent } from './components/bidding-price/bidding-price.component';
+
+const routes: Routes = [
+  {
+    path:'', component:LoginComponent
+  },
+  {
+    path:'login', component:LoginComponent
+  },
+  {
+    path:'home', component:HomeComponent
+  },
+  
+  {
+    path:'register', component:RegisterComponent
+  },
+  {
+    path:'admin-login', component:AdminLoginComponent
+  },
+  {
+    path:'add-car', component:AddCarComponent
+  },
+  {
+    path:'owner-details', component:OwnerDetailsComponent
+  },
+  {
+    path:'car-in-bidding', component:CarInBiddingComponent
+  },
+  {
+    path:'car-sold', component:CarSoldComponent
+  },
+  {
+    path:'bidding-price', component:BiddingPriceComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/components/bidding-price/bidding-price.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bidding-price',
+  templateUrl: './bidding-price.component.html',
+  styleUrls: ['./bidding-price.component.css']
+})
+export class BiddingPriceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
